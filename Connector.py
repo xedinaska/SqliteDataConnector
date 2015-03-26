558b4d57dc8d3ed73956f166ea2b4890c00b3778
Alex <Xedinaska@gmail.com>
2015-03-27 02:00:14 +0300

    Added few comment lines to Connector class

--- Connector.py
+++ Connector.py
@@ -8,15 +8,18 @@ class Connector:
     required_fields = None
     fields_to_fetch = 'config/fields.json'
 
+    #init db connection
     def __init__(self, db_name):
         self.connection = sqlite3.connect(db_name)
 
         with open(self.fields_to_fetch) as json_data:
             self.required_fields = json.load(json_data)
 
+    #close db connection
     def disconnect(self):
         self.connection.close()
 
+    #get db tables list
     def get_tables(self):
         cur = self.connection.cursor()
 
@@ -32,6 +35,7 @@ class Connector:
 
         return tables
 
+    #get db table fields list
     def get_table_structure(self, table_name):
         cur = self.connection.cursor()
 
@@ -48,6 +52,7 @@ class Connector:
 
         return table_fields
 
+    #get db table content data
     def get_table_content(self, table_name):
         cur = self.connection.cursor()
         select_string = '*'
